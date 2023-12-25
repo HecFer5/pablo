7e55c47ba12dd6cd0364bd45142aa230d52e61b3
HecFer5 <hecalfer@hotmail.com>
2023-12-25 11:51:58 -0300

    agregando el calendario

--- src/paginas/Portada.jsx
+++ src/paginas/Portada.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import dayjs from 'dayjs'
+import "dayjs/locale/es"
+dayjs.locale('es')
 
 const Portada = () => {
+  const localizer = dayjsLocalizer(dayjs)
+
+
+
+  
+  const events = [
+    {
+      start: dayjs('2023-12-18T12:00:00').toDate(),
+      end: dayjs('2023-12-18T13:10:00').toDate(),
+      title: 'evento 1'
+    }
+  ]
   return (
 
-
-
-    <div id="default-carousel" className="relative w-full p-3" data-carousel="slide">
-      <div className="relative h-56 overflow-hidden rounded-lg md:h-96 bg-slate-400 p-3">
-        <h1>djd</h1>
-      </div>
+    <div className='container mx-auto sm mt-10'>
+      <Calendar
+        localizer={localizer}
+        style={{ height: 500 }}
+        events={events}
+        defaultView="week"
+      />
     </div>
+
 
   )
 }
